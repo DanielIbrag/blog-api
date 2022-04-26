fb21902f9ceb4e37d33fde7c5beeae8ffe306b8e
DanielIbrag <danny.ibragimov1@gmail.com>
2022-04-25 20:47:28 -0400

    adding index test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
     it "works! (now write some real specs)" do
-      get posts_index_path
+      user = User.create!(name: "Peter", email: "peter@test.com", password: "password")
+      Post.create!(user_id: user.id, title: Faker::Company.catch_phrase, body: Faker::Hipster.paragraph(sentence_count: 6), image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I")
+      get "/posts"
+      posts = JSON.parse(response.body)
       expect(response).to have_http_status(200)
+      expect(posts.length).to eq(1)
     end
   end
 end
